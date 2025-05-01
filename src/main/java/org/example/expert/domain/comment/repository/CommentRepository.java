434c66e0bd407aa7dbd59cbeb09582967ab7f3da
nagul2 <hayani0000@gmail.com>
2025-05-01 15:58:37 +0900

    필수[LV2]: 7. getComments()메서드 N+1 해결
    1. Page객체로 반환하지 않으므로 fetch join으로 해결

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    @Query("select c from Comment c join fetch c.user where c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
